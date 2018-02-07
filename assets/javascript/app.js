34262fb7c17ba6dd2a8a10dbec5441a2d1f7eadb
jsalamat <salamat_jon@yahoo.com>
2018-02-07 12:42:23 -0500

    base settings strokestyle linecap and linejoin

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,14 @@ const ctx = canvas.getContext('2d');
 // size the canvas by width of the window
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// base settings stroke style, line cap, and line join
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+// when draw it require a color here start with #BADA55
+// lineJoin and lineCap are rounded at start and end of line
+
+
+
