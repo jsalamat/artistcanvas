1f942637cdda85444637c827b434510ac7445c14
jsalamat <salamat_jon@yahoo.com>
2018-02-07 12:30:41 -0500

    setting up canvas and making it a 2d context

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,9 @@
-alert("Javascript and jQuery works!");
-console.log('you can erase this if alert works');
+// select canvas
+const canvas = document.querySelector('#draw');
+
+//create 2d context which can only be draw on
+const ctx = canvas.getContext('2d');
+
+// size the canvas by width of the window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
