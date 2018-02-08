808ae4229b187b474def3c104a775948f0ddec77
jsalamat <salamat_jon@yahoo.com>
2018-02-08 10:12:45 -0500

    increment hue and line width as flip direction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,24 +15,30 @@ ctx.lineCap = 'round';
 //line width edit
 ctx.lineWidth = 100;
 
+// https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+// ctx.globalCompositeOperation = 'multiply';
 
 // when draw it require a color here start with #BADA55
 // lineJoin and lineCap are rounded at start and end of line
 
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-
 // isDrawing is setting false
 	// when click down setting is true
 	// when click up (let go of button) set to false
 // lastX and lastY need starting x,y and ending x,y
+let lastX = 0;
+let lastY = 0;
+// starting point of hue
+let hue = 0;
+// start direction to build up
+let direction = true;
 
 // draw function that take in event
-
 function draw(e) {
 	if(!isDrawing) return; //stop the fn from runnig when not mouse down
 	console.log(e)
+	// As we draw we set it to hsl
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
 // ctx involves drawing for canvas
 // need to start x,y and move to x,y
@@ -44,6 +50,24 @@ function draw(e) {
 	ctx.stroke();
 // update the lastx and lasty variables
 	[lastX, lastY] = [e.offsetX, e.offsetY];
+
+	// increment hue on each stroke;
+	hue++;
+	if (hue >= 360) {
+		hue = 0;
+	}
+
+	// for every loop to increment a line width
+	// if line width greater 100 or less than 1 the direction is flip
+	// if direction is true the line width goes up other wise it goes down
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 };
 
 canvas.addEventListener('mousemove', draw);
